6e26c9ddc7e8e8e77a2c32abc4c356dbb7cc22ab
Artyom Silantiev <artyom.silantiev@gmail.com>
2018-11-27 14:57:55 +0300

    Update package.json

--- front/components/mania/mania.component.ts
+++ front/components/mania/mania.component.ts
@@ -1,26 +1,21 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mania',
-  templateUrl: './mania.component.html',
-  styleUrls: ['./mania.component.scss']
+    selector: 'app-mania',
+    templateUrl: './mania.component.html',
+    styleUrls: ['./mania.component.scss']
 })
-
 export class ManiaComponent implements OnInit {
-  private res: number;
+    private res: number;
 
-  constructor() {
-  }
+    constructor() {}
 
-  ngOnInit(): void {
-    this.res = Number(localStorage.getItem('mania.res')) || 0;
-  }
+    ngOnInit(): void {
+        this.res = Number(localStorage.getItem('mania.res')) || 0;
+    }
 
-  private moreRes(): void {
-    this.res += 10;
-    localStorage.setItem('mania.res', String(this.res));
-  }
+    private moreRes(): void {
+        this.res += 10;
+        localStorage.setItem('mania.res', String(this.res));
+    }
 }
